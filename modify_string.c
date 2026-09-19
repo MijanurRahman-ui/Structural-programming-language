49580ae7d86f233a5f56955a4527629e12f347f1
Mijanur Rahman-07 <mijanur973645@gmail.com>
2026-09-19 20:29:23 +0600

    Add files via upload

---
+++ modify_string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+ char ch[]="Hello world!";
+ ch[1]='A';
+ printf("%s", ch);
+
+ return 0;
+}
---
+++ simple_welcome.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+ char massege[] = "Good to see you ";
+ char name[] = "Rony ";
+
+ printf("%s%s", massege, name);
+ return 0;
+}
