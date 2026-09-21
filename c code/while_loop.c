26787cfd1936a4fa165d1b0ab428de4ce3c6d1ca
Mijanur Rahman-07 <mijanur973645@gmail.com>
2026-09-21 23:17:47 +0600

    table in c code

---
+++ c code/while_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+ int n;
+ printf("Enter number ");
+ scanf("%d",&n);
+ int i=1;
+ while(i<=10)
+ {
+  printf("%d X %d = %d \n",n,i,n*i);
+  i++;
+ }
+
+ return 0;
+}
