aab81474a5cb306a43bddf86cd6960007e77e1dc
Mijanur Rahman-07 <mijanur973645@gmail.com>
2026-09-22 15:26:51 +0600

    small to capitam AND capital to small latter

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//  char ch[100];
+//  printf("Enter string ");
+//  fgets(ch, sizeof(ch), stdin);
+//  int len = strlen(ch);
+//  for (int i = 0; i < len; i++)
+//  {
+//   if (ch[i] >= 'a' && ch[i] <= 'z')
+//   {
+//    ch[i] -= 32;
+//   }
+//   else if (ch[i] >= 'A' && ch[i] <= 'Z')
+//   {
+//    ch[i] += 32;
+//   }
+//  }
+//  puts(ch);
+
+//  return 0;
+// }
